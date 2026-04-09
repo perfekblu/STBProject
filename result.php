272327b4102946394c914bf8f8ee29b4869719c2
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-09 02:20:16 -0400

    php test

---
+++ result.php
@@ -0,0 +1,29 @@
+!<DOCTYPE html>
+<html>
+    <head>
+        <?php
+            $server = "localhost";
+            $username = "aba";
+            $password = "abab";
+            $database = "STBProject";
+            $conn = mysqli_connect($server, $username, $password, $database);
+            // Check connection
+            if (!$conn) {
+                die ("Connection failed: {mysqli_connector_error()}");
+            }
+            echo "Connected successfully";
+            $sql = "select * from Pusers;";
+            $result = mysqli_query ($conn, $sql);
+
+            $name = htmlspecialchars($_POST['name']);
+        ?>
+    </head>
+    <body>
+        <?php
+            $sql = "INSERT INTO Pusers (name) values ('$name');";
+            $result = mysqli_query($conn, $sql);
+
+            mysqli_close($conn);
+        ?>
+    </body>
+</html>
