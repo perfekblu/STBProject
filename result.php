a628e8d30ca1910e066be6a992ac38cc735bf5d6
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-12 02:12:37 -0400

    sqlajax

--- result.php
+++ result.php
@@ -1,36 +1,30 @@
-!<DOCTYPE html>
-<html>
-    <head>
-        <?php
-            $server = "localhost";
-            $username = "aba";
-            $password = "abab";
-            $database = "STBProject";
-            $conn = mysqli_connect($server, $username, $password, $database);
-            // Check connection
-            if (!$conn) {
-                die ("Connection failed: {mysqli_connector_error()}");
-            }
-            echo "Connected successfully";
+<?php
+    $server = "localhost";
+    $username = "aba";
+    $password = "abab";
+    $database = "STBProject";
+    $conn = mysqli_connect($server, $username, $password, $database);
+    // Check connection
+    if (!$conn) {
+        die ("Connection failed: {mysqli_connector_error()}");
+    }
+    echo "Connected successfully";
 
-            $sql = "select * from Pusers;";
-            $result = mysqli_query ($conn, $sql);
+    $sql = "select * from Pusers;";
+    $result = mysqli_query ($conn, $sql);
 
-            $name = htmlspecialchars($_POST['name']);
-        ?>
-    </head>
-    <body>
-        <?php
-            $sql = "INSERT INTO Pusers (names) values ('$name');";
-            $result = mysqli_query($conn, $sql);
+    /*$name = htmlspecialchars($_POST['name']);
+
+    $sql = "INSERT INTO Pusers (names) values ('$name');";
+    $result = mysqli_query($conn, $sql);*/
             
-            $result = $conn->query("SELECT * FROM Pusers");
+    $result = $conn->query("SELECT * FROM Pusers");
 
-            while($row = $result->fetch_assoc()) {
-                echo $row["names"] . "<br>";
-            }
+    while($row = $result->fetch_assoc()) {
+        echo $row['Names'] . "\t". ;
+        echo $row['Age'] . "<br>";
+    }
 
-            mysqli_close($conn);
-        ?>
-    </body>
-</html>
+    mysqli_close($conn);
+?>
+
