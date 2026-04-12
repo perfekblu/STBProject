7e8f6a648456722d639d7051bedab1b5cae45cb6
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-12 02:31:25 -0400

    example test

---
+++ family.php
@@ -0,0 +1,28 @@
+<?php
+    $q = intval($_GET['q']);
+
+    $con = mysqli_connect('localhost','aba','abab');
+    if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+    }
+
+    mysqli_select_db($con,"STBProject");
+    $sql="SELECT * FROM user WHERE id = '".$q."'";
+    $result = mysqli_query($con,$sql);
+
+    echo "<table>
+    <tr>
+    <th>Names</th>
+    <th>Age</th>
+    </tr>";
+    while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['Names'] . "</td>";
+
+    echo "<td>" . $row['Age'] . "</td>";
+
+    }
+    echo "</table>";
+
+    mysqli_close($con);
+?>
