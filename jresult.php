cbd4fcb5403288d273df5105f1531e41be508cb4
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-16 16:16:58 -0400

    jresult1

--- jresult.php
+++ jresult.php
@@ -10,18 +10,7 @@ $json = json_decode($data, true);
 </head>
 <body>
     <h1>Sensor Data</h1>
-    <script>
-        setInterval(() => {
-            fetch('data.json')
-            .then(res => res.json())
-            .then(data => {
-                document.getElementById("temp").innerText = data.temperature;
-                document.getElementById("hum").innerText = data.humidity;
-            });
-        }, 2000);
-</script>
-
-<p>Temperature: <span id="temp"></span></p>
-<p>Humidity: <span id="hum"></span></p>
+    <p>Temperature: <?php echo $json['temperature']; ?> °C</p>
+    <p>Humidity: <?php echo $json['humidity']; ?> %</p>
 </body>
 </html>
