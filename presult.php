69cfccaae6a27afcaf093736dda9dd48c14c0cfa
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-16 13:32:02 -0400

    presult.php

---
+++ presult.php
@@ -0,0 +1,7 @@
+<?php
+    $json = file_put_contents('php://input');
+    $data = json_decode($json,true);
+
+    echo "Temperature: " . $data['temp'] . <br>;
+    echo "Humidity: " . $data['hum'] . <br>;
+?>
