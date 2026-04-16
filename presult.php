43f1b303396f33be3e4f7c0bdf6b938608fa5c39
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-16 16:37:41 -0400

    presultmaj

--- presult.php
+++ presult.php
@@ -1,8 +1,46 @@
 <?php
+    //Connect to database
+    $server = "localhost";
+    $username = "aba";
+    $password = "abab";
+    $database = "STBProject";
+    $conn = mysqli_connect($server, $username, $password, $database);
+    // Check connection
+    if (!$conn) {
+        echo "Connection failed";
+        die ("Connection failed: {mysqli_connector_error()}");
+    }
+    echo "Connected successfully"."<br>";
+
+    
     $json = file_get_contents('php://input');
     $data = json_decode($json,true);
-    echo file_get_contents('php://input') . "<br>" . "<br>";
+    //echo file_get_contents('php://input') . "<br>" . "<br>";
 
-    echo "Temperature: " . $data['temp'] . "<br>";
-    echo "Humidity: " . $data["hum"] . "<br>";
+    $temp = $data['temp'];
+    $hum = $data['hum'];
+
+    if(!empty($_POST)){
+        $sql = "INSERT INTO sensor_test (temperature,humidity) values ($temp,$hum);";
+        $result = mysqli_query($conn, $sql);
+    }
+    
+    //Display all current users
+    $result = $conn->query("SELECT * FROM sensor_test;");
+
+    /*while($row = $result->fetch_assoc()) {
+        echo $row['Names'] . "\t";
+        echo $row['Age'] . "<br>";
+    }*/
+
+    $row = $result->fetch_assoc();
+
+    echo $row['temperature'] . "°C" . "<br>";
+    echo $row['humidity'] . "%" . "<br>";
+    echo $row['created_at'] . "<br>";
+
+    mysqli_close($conn);
+
+    //echo "Temperature: " . $data['temp'] . "<br>";
+    //echo "Humidity: " . $data["hum"] . "<br>";
 ?>
