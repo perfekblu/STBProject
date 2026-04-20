e38368596fb6a00289ec097d297a9b3048709dde
Steven L. <steven.luumk@gmail.com>
2026-04-20 14:55:11 -0400

    jsdatafix1

--- jsdata.php
+++ jsdata.php
@@ -1,6 +1,9 @@
 <?php
     $json = file_get_contents('php://input');
-    echo "$json";
+    $data = json_decode($json,true)
+
+    echo json_encode($data);
+
 
     $temp = $data['temp'];
     $hum = $data['hum'];
