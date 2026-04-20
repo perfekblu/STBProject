da99f3ab44d84f7f904015c553c63f50bb801a69
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-20 18:12:07 -0400

    jsdatafix11

--- jsdata.php
+++ jsdata.php
@@ -2,6 +2,7 @@
     $json = file_get_contents('php://input');
     $data = json_decode($json,true);
     //echo "$json";
+    header('Content-Type: application/json');
     echo json_encode($data);
 
 
