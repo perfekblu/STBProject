c8f4bb0fcbe377b6d4df031ea3fcc8af6e840c68
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-23 16:38:42 -0400

    trygraphtemp

---
+++ script.js
@@ -0,0 +1,11 @@
+let x;
+
+
+function dis() {
+                            if (x >= 1000) {
+                                document.getElementById("alert").style.display = "";
+                            }
+                            else {
+                                document.getElementById("alert").style.display = "none";
+                            }
+                        };  
