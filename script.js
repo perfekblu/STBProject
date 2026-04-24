4cf24420c392faaa838f91b7108de0b0ef28e391
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-24 02:07:49 -0400

    scri[t

--- script.js
+++ script.js
@@ -1,58 +1,23 @@
+let x;
+                        
+setInterval(async function getData() {
+    try {
+        const response = await fetch('presult.php');
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json(); // Parses JSON response from PHP
+        console.log(data);
+                    
+        x = `${data.tvoc}`;
 
-
-
-//AREA
-var options = {
-          series: [{
-          name: 'TEAM A',
-          type: 'area',
-          data: [44, 55, 31, 47, 31, 43, 26, 41, 31, 47, 33]
-        }, {
-          name: 'TEAM B',
-          type: 'line',
-          data: [55, 69, 45, 61, 43, 54, 37, 52, 44, 61, 43]
-        }],
-          chart: {
-          height: 350,
-          type: 'line',
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        fill: {
-          type:'solid',
-          opacity: [0.35, 1],
-        },
-        labels: ['Dec 01', 'Dec 02','Dec 03','Dec 04','Dec 05','Dec 06','Dec 07','Dec 08','Dec 09 ','Dec 10','Dec 11'],
-        markers: {
-          size: 0
-        },
-        yaxis: [
-          {
-            title: {
-              text: 'Series A',
-            },
-          },
-          {
-            opposite: true,
-            title: {
-              text: 'Series B',
-            },
-          },
-        ],
-        tooltip: {
-          shared: true,
-          intersect: false,
-          y: {
-            formatter: function (y) {
-              if(typeof y !== "undefined") {
-                return  y.toFixed(0) + " points";
-              }
-              return y;
-            }
-          }
+        } catch (error) {
+            console.error('Fetch error:', error);
         }
-        };
 
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        chart.render();
+        if (x >= 1000) {
+            document.getElementById("alert").style.display = "";
+        }
+        else {
+            document.getElementById("alert").style.display = "none";
+        }
+},2000);
+                            
