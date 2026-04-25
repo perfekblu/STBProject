26d32c613690312bd7510bf233f3357114d75cb2
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-25 06:00:34 -0400

    sripajpy

--- script.js
+++ script.js
@@ -7,13 +7,7 @@ setInterval(async function getData() {
         const data = await response.json(); // Parses JSON response from PHP
         console.log(data);
 
-        document.getElementById('temp').innerHTML = `${data.temperature}°C`;
-        document.getElementById('hum').innerHTML = `${data.humidity}%`;
-        document.getElementById('gas').innerHTML = `${data.gas}PPM`;
-        document.getElementById('tvoc').innerHTML = `${data.tvoc}PPB`;
-        document.getElementById('aqi').innerHTML = `${data.aqi}`;
-        document.getElementById('cap').innerHTML = `${data.capacity}%`;
-        document.getElementById('time').innerHTML = `Created at: ${data.created_at}`;
+
                     
         x = `${data.tvoc}`;
 
