5043f283c719ab9ac09f9bfea94ce97862a47c61
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-04-27 02:22:16 -0400

    alarma<=>data

--- alarm.js
+++ alarm.js
@@ -1,20 +1,22 @@
+let x;
+                        
 setInterval(async function getData() {
     try {
         const response = await fetch('presult.php');
         if (!response.ok) throw new Error('Network response was not ok');
         const data = await response.json(); // Parses JSON response from PHP
         console.log(data);
-
-        document.getElementById('temp').innerHTML = `${data.temperature}°C`;
-        document.getElementById('hum').innerHTML = `${data.humidity}%`;
-        document.getElementById('gas').innerHTML = `${data.gas}PPM`;
-        document.getElementById('tvoc').innerHTML = `${data.tvoc}PPB`;
-        document.getElementById('aqi').innerHTML = `${data.aqi}`;
-        document.getElementById('cap').innerHTML = `${data.capacity}%`;
-        document.getElementById('time').innerHTML = `Created at: ${data.created_at}`;
+                    
+        x = `${data.tvoc}`;
 
     } catch (error) {
         console.error('Fetch error:', error);
     }
-},2000);
 
+    if (x >= 1000) {
+        document.getElementById("alert").style.display = "";
+    }
+    else {
+        document.getElementById("alert").style.display = "none";
+    }
+},2000);
--- data.js
+++ data.js
@@ -1,23 +1,20 @@
-let x;
-                        
 setInterval(async function getData() {
     try {
         const response = await fetch('presult.php');
         if (!response.ok) throw new Error('Network response was not ok');
         const data = await response.json(); // Parses JSON response from PHP
         console.log(data);
-                    
-        x = `${data.tvoc}`;
+
+        document.getElementById('temp').innerHTML = `${data.temperature}°C`;
+        document.getElementById('hum').innerHTML = `${data.humidity}%`;
+        document.getElementById('gas').innerHTML = `${data.gas}PPM`;
+        document.getElementById('tvoc').innerHTML = `${data.tvoc}PPB`;
+        document.getElementById('aqi').innerHTML = `${data.aqi}`;
+        document.getElementById('cap').innerHTML = `${data.capacity}%`;
+        document.getElementById('time').innerHTML = `Created at: ${data.created_at}`;
 
     } catch (error) {
         console.error('Fetch error:', error);
-    }
-
-    if (x >= 1000) {
-        document.getElementById("alert").style.display = "";
-    }
-    else {
-        document.getElementById("alert").style.display = "none";
     }
 },2000);
 
