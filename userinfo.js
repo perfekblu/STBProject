bddce65dc36425b4395f8c140e92979e17740879
Steven L. <steven.luumk@gmail.com>
2026-05-01 13:07:41 -0400

    userinfostr1

--- userinfo.js
+++ userinfo.js
@@ -1,7 +1,7 @@
 async function userinfo(str) {
     try {
         //Get user data from database
-        const response = await fetch('fun.php?q='+str,true);
+        const response = await fetch('userinfo.php?q='+str,true);
         if (!response.ok) throw new Error('Network response was not ok');
         const data = await response.text(); // Parses JSON response from PHP
         console.log(data);
