4a60da8e4d79826c32f52811d38ace28b02f7f6c
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-05-05 22:59:25 -0400

    userinfotrouble1

--- userinfo.php
+++ userinfo.php
@@ -20,10 +20,11 @@
     $sql = "SELECT * FROM users_login WHERE user_id = '".$q."'";
     $result = mysqli_query($conn,$sql);
 
+    echo "$result";
 
-    while($row = $result->fetch_assoc()) {
+    /*while($row = $result->fetch_assoc()) {
         $tab[] = $row["name"], $row["login"];
-    }
+    }*/
 
     echo json_encode($tab);
 
