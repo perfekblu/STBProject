f2c4b753ab6832324c05a7c1b11a9382d4a90965
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-05-06 00:35:19 -0400

    userinfophpjson

--- userinfo.php
+++ userinfo.php
@@ -1,7 +1,7 @@
 <?php
     $q = intval($_POST['q']);
     
-    echo "$q";
+    //echo "$q";
 
     //Connect to database
     $server = "localhost";
@@ -20,12 +20,13 @@
     $sql = "SELECT * FROM users_login WHERE user_id = '".$q."'";
     $result = mysqli_query($conn,$sql);
 
-    while($row = $result->fetch_assoc()) {
+   /* while($row = $result->fetch_assoc()) {
         //$tab[] = $row["name"], $row["login"];
         echo "Name: " . $row["name"] . " - Login: " . $row["login"] . "<br>";
-    }
+    }*/
 
-    //echo json_encode($tab);
+    $row = $result->fetch_assoc();
+    echo json_encode($row);
 
     mysqli_close($conn);
 
