34040d0e030928611a92bcc306e74c72393a791f
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-05-06 02:53:47 -0400

    userinfondatalet

--- userinfo.js
+++ userinfo.js
@@ -11,7 +11,7 @@ async function userinfo(str) {
         //Get user data from database
         const response = await fetch('userinfo.php',options);
         if (!response.ok) throw new Error('Network response was not ok');
-        const data = await response.json(); // Parses JSON response from PHP
+        let data = await response.json(); // Parses JSON response from PHP
         console.log(data);
 
         //document.getElementById("usertxt").innerHTML = `${data.fun}`;
