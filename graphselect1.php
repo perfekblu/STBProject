eadc8e5c08ffcfc59c7527811de328606098e563
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-05-14 16:43:40 -0400

    graphsel21

---
+++ graphselect1.php
@@ -0,0 +1,40 @@
+<?php
+    $q = $_POST['q'];
+
+    //echo "$q";
+
+    
+    $server = "localhost";
+    $username = "aba";
+    $password = "abab";
+    $database = "STBProject";
+    $conn = mysqli_connect($server, $username, $password, $database);
+    // Check connection
+    if (!$conn) {
+        echo "Connection failed";
+        die ("Connection failed: {mysqli_connector_error()}");
+    }
+
+    //$result = $conn->query("SELECT $q, 
+    //DATE_FORMAT(created_at, '%H:%i:%s') as time FROM sensor_data ORDER BY id DESC LIMIT 30");
+
+    //$stmt = $mysqli->prepare("SELECT $q FROM sensor_data ORDER BY id DESC LIMIT 30");
+    //$stmt->bind_param("s", $q); // "s" = string, "i" = integer, "d" = double
+    //$stmt->execute();
+    //$result = $stmt->get_result();
+
+    $sql = "SELECT $q as value,
+    DATE_FORMAT(created_at, '%H:%i:%s') as time FROM sensor_data ORDER BY id DESC LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+
+
+    while ($row = mysqli_fetch_assoc($result)){
+        $data[] = $row;
+    }
+    //$row = $result->fetch_assoc();
+
+    echo json_encode($data);
+
+    mysqli_close($conn);
+
+?>
