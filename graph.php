c8ff39562033ebb5c743d7b3d53b11f8c0f0e18d
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2026-05-14 16:37:10 -0400

    graphsel18.1

--- graph.php
+++ graph.php
@@ -3,7 +3,7 @@
 
     //echo "$q";
 
-
+    
     $server = "localhost";
     $username = "aba";
     $password = "abab";
@@ -15,18 +15,25 @@
         die ("Connection failed: {mysqli_connector_error()}");
     }
 
-    //$result = $conn->query("SELECT gas, 
-    //DATE_FORMAT(created_at, '%H:%i:%s') as time FROM sensor_data ORDER BY id DESC LIMIT 1");
+    //$result = $conn->query("SELECT $q, 
+    //DATE_FORMAT(created_at, '%H:%i:%s') as time FROM sensor_data ORDER BY id DESC LIMIT 30");
+
+    //$stmt = $mysqli->prepare("SELECT $q FROM sensor_data ORDER BY id DESC LIMIT 30");
+    //$stmt->bind_param("s", $q); // "s" = string, "i" = integer, "d" = double
+    //$stmt->execute();
+    //$result = $stmt->get_result();
+
     $sql = "SELECT $q as value,
     DATE_FORMAT(created_at, '%H:%i:%s') as time FROM sensor_data ORDER BY id DESC LIMIT 1";
     $result = mysqli_query($conn, $sql);
+
 
     while ($row = mysqli_fetch_assoc($result)){
         $data[] = $row;
     }
     //$row = $result->fetch_assoc();
 
-    echo json_encode($row);
+    echo json_encode($data);
 
     mysqli_close($conn);
 
